b02e62a821a6585449c09947084603aa76c3ec0b
Mihreteab Bizuayehu <lijmihret@gmail.com>
2024-08-04 07:52:46 +0300

    Python Fundamental
    It is focus on creating different scenarios (simple projects) to improve python skills

---
+++ data-structure/forloop.py
@@ -0,0 +1,66 @@
+# numbers=[3,4,1,2,5]
+# print(numbers[3])
+# numbers.sort()
+# for number in numbers:
+#     print(number)
+# for i in range(1,100):
+#     print(i)
+# numbers = [1, 2, 3, 4, 5]
+# total = 0
+# for number in numbers:
+#     total += number
+# print("The sum is:", total)
+# firstName=""
+# import random
+
+# number_to_guess = random.randint(1, 10)
+# guess = None
+
+# while guess != number_to_guess:
+#     guess = int(input("Guess a number between 1 and 10: "))
+#     if guess < number_to_guess:
+#         print("Too low!")
+#     elif guess > number_to_guess:
+#         print("Too high!")
+# print("Congratulations! You guessed the correct number.")
+
+# for i in range(5):
+#     for j in range(5):
+#         print('*', end=' ')
+#     print()
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# rows = 5
+# for i in range(1, rows + 1):
+#     for j in range(rows - i):
+#         print(' ', end='')
+#     for k in range(2 * i - 1):
+#         print('*', end='')
+#     print()
+
+# for i in range(1, 11):
+#     if i == 5:
+#         break
+#     print(i)
+
+# for i in range(1, 11):
+#     if i == 5:
+#         continue
+#     print(i)
+
+# def describe_pet(animal_type, pet_name="Fluffy"):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name}.")
+
+# # Usage
+# describe_pet('hamster', 'Harry')
+# describe_pet('dog')
+
+# my_dict = {('a', 1): 'value1', ('b', 2): 'value2'}
+# print(my_dict)  # Output: {('a', 1): 'value1', ('b', 2): 'value2'}
+
