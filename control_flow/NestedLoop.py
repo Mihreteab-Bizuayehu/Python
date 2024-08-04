24011f8cde3561f6a17ecef41151faaf6cf3f07a
Mihreteab Bizuayehu <lijmihret@gmail.com>
2024-08-04 22:10:11 +0300

    Nested loop is Updated

--- control_flow/NestedLoop.py
+++ control_flow/NestedLoop.py
@@ -1,25 +1,24 @@
-# 
-# # Pattern printing using nested loop.
-# rows = 5
-# for i in range(1, rows + 1):
-#     for j in range(rows - i):
-#         print(' ', end='')
-#     for k in range(2 * i - 1):
-#         print('*', end='')
-#     print()
+# Pattern printing using nested loop.
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(rows - i):
+        print(' ', end='')
+    for k in range(2 * i - 1):
+        print('*', end='')
+    print()
 
 
 
 fruits=["apple","lemon","orange","mango"]
 
-# print(fruits[3])
-# fruits.append("abocado")
-# print(fruits)
-# fruits.remove("lemon")
-# print(fruits)
+print(fruits[3])
+fruits.append("abocado")
+print(fruits)
+fruits.remove("lemon")
+print(fruits)
 
-# squers=[x**2 for x in range(10)]
-# print(squers)
+squers=[x**2 for x in range(10)]
+print(squers)
 
 
 
